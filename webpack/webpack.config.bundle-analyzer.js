b2d9ce5e9f8e74aa43f1b6b01b83273a295519af
Alexey Tuboltsev <tblz@palavara.com>
2019-01-30 15:06:13 +0100

    add analyzer mode to webpack

--- webpack/webpack.config.bundle-analyzer.js
+++ webpack/webpack.config.bundle-analyzer.js
@@ -4,7 +4,7 @@ const merge = require("webpack-merge");
 const prodConfig = require("./webpack.config.prod");
 
 const bundleAnalyzerConfig = merge(prodConfig, {
-  plugins: [new BundleAnalyzerPlugin({logLevel: "silent"})],
+  plugins: [new BundleAnalyzerPlugin({logLevel: "silent", analyzerMode:"static"})],
 });
 
 module.exports = bundleAnalyzerConfig;
