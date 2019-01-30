14401b81d7db7cb442c578f54d91ab6865b6dc03
Alexey Tuboltsev <tblz@palavara.com>
2019-01-30 17:41:07 +0100

    wip

--- webpack/webpack.config.bundle-analyzer.js
+++ webpack/webpack.config.bundle-analyzer.js
@@ -3,8 +3,11 @@ const merge = require("webpack-merge");
 
 const prodConfig = require("./webpack.config.prod");
 
+const date = new Date();
+const formattedDate = `${date.getMonth()-1}_${date.getDate()}_${date.getHours()}:${date.getMinutes()}`;
+
 const bundleAnalyzerConfig = merge(prodConfig, {
-  plugins: [new BundleAnalyzerPlugin({logLevel: "silent", analyzerMode:"static"})],
+  plugins: [new BundleAnalyzerPlugin({reportFilename:`report${formattedDate}.html`,logLevel: "silent", analyzerMode:"static"})],
 });
 
 module.exports = bundleAnalyzerConfig;
